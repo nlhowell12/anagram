566b5ab9a369fd18f7be6d1739997ae950354739
Nick Howell <nlhowell12@gmail.com>
2018-02-08 14:11:17 -0500

    Complete

---
+++ main.js
@@ -0,0 +1,29 @@
+document.getElementById("findButton").onclick = function() {
+    // your code will go here ...
+    var typedText = document.getElementById("input").value;
+    typedText = typedText.toLowerCase().split("").sort().join("").trim();
+    var testWord = '';
+    var output = [];
+
+    function alphabetize(a) {
+        return a.toLowerCase().split("").sort().join("").trim();
+    }
+        for (let i=0; i<words.length; i++){
+            testWord = alphabetize(words[i]);
+            if (testWord === typedText){
+                output.push(words[i]);
+            }
+        }
+
+        for (let o=0; o<output.length; o++) {
+        var span = document.createElement("div");
+        var textContent = document.createTextNode(output[o]);
+        span.appendChild(textContent);
+        document.getElementById("wordsDiv").appendChild(span);
+        }
+        
+    }
+    
+    
+
+
