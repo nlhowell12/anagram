9811f103feb1f5538e7c6bea49404ed9c3f88d35
Nick Howell <nlhowell12@gmail.com>
2018-03-20 12:02:19 -0400

    changed to find all anagrams with 5 or more matches

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
-document.getElementById("findButton").onclick = function() {
+var anagramSets = {
+
+};
+
+document.getElementById("findButton").onclick = function () {
     // your code will go here ...
-    var typedText = document.getElementById("input").value;
-    typedText = typedText.toLowerCase().split("").sort().join("").trim();
     var testWord = '';
     var output = [];
 
     function alphabetize(a) {
         return a.toLowerCase().split("").sort().join("").trim();
     }
-        for (let i=0; i<words.length; i++){
-            testWord = alphabetize(words[i]);
-            if (testWord === typedText){
-                output.push(words[i]);
-            }
-        }
 
-        for (let o=0; o<output.length; o++) {
-        var span = document.createElement("div");
-        span.id = "anaOut"
-        var textContent = document.createTextNode(o + "." + " "+ output[o]);
-        span.appendChild(textContent);
-        document.getElementById("wordsDiv").appendChild(span);
+    for (let i = 0; i < words.length; i++) {
+        testWord = alphabetize(words[i]);
+        if (!anagramSets[testWord]) {
+            anagramSets[testWord] = [];
+        }
+        anagramSets[testWord].push(words[i]);
+    }
+
+    for (testWord in anagramSets) {
+        let length = anagramSets[testWord].length;
+        let output = anagramSets[testWord];
+        let wordsDiv = document.getElementById('wordsDiv');
+        let newDiv = document.createElement('div');
+
+        if (length >= 5) {
+            wordsDiv.appendChild(newDiv);
+            newDiv.innerHTML = (output.toString());
         }
     }
-    
-    
-
-
+}
